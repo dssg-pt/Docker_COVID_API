e3f99f3543137633e0923a3bb14d111b18801095
tiago_freitas <tiago.freitas@nilg.ai>
2020-04-16 17:25:13 +0100

    documentation updated to be main english with portuguese translation added

--- src/flask_server.py
+++ src/flask_server.py
@@ -17,10 +17,14 @@ name_space = app.namespace('Requests', description='Request disponíveis até à
 class GetLastUpdate(Resource):
 
     def get(self):
-        """ Retorna o último update do dataset em formato JSON.
+        """ Returns the last updated entry
+
+            Returns a dict with the following fomat: {index -> {column -> value}}
+
+            PT: Retorna o último update do dataset em formato JSON.
 
             Retorna um dicionário em formato JSON do tipo: {index -> {column -> value}}
-
+    
         """
         url = 'https://raw.githubusercontent.com/dssg-pt/covid19pt-data/master/data.csv'
         df = pd.read_csv(url, error_bad_lines=False)
@@ -32,7 +36,11 @@ class GetLastUpdate(Resource):
 class GetFullDataset(Resource):
 
     def get(self):
-        """ Retorna o dataset inteiro em formato JSON.
+        """ Returns the full dataset in a JSON format.
+
+            Returns a dict with the following fomat: {index -> {column -> value}}
+
+            PT: Retorna o dataset inteiro em formato JSON. 
 
             Retorna um dicionário em formato JSON do tipo: {index -> {column -> value}}
 
@@ -45,12 +53,18 @@ class GetFullDataset(Resource):
 @name_space.route('/get_entry/<string:date>')
 class GetSpecificDate(Resource):
     
-    @app.doc(responses={ 200: 'OK', 500: 'Data Não encontrada no dataset' }, 
-			 params={ 'date': 'Especifica a data desejada no formato dd/mm/yyyy' })
+    @app.doc(responses={ 200: 'OK', 500: 'Requested data was not found.' }, 
+			 params={ 'date': 'Specify the date in the format dd/mm/yyyy' })
     def get(self, date):
-        """ Retorna o update para uma data específica
+        """ Returns the update of a specific date
 
-            Deve ser feito no formato dia-mês-ano. Por exemplo /get_entry/01-04-2020
+            Should be asked in the following format: dd-mm-yyyy. For example: /get_entry/01-04-2020
+
+            Returns a dict with the following fomat: {index -> {column -> value}}
+
+            PT: Retorna o update para uma data específica
+
+            Deve ser feito no formato dd-mm-yyyy. Por exemplo /get_entry/01-04-2020
 
             Retorna um dicionário em formato JSON do tipo: {index -> {column -> value}}
 
@@ -63,20 +77,26 @@ class GetSpecificDate(Resource):
         entry_of_interest = df.loc[df.data == date]
 
         if entry_of_interest.shape[0] == 0:
-            name_space.abort(500, status = "Data Não encontrada no dataset", statusCode = "500")
+            name_space.abort(500, status = "Requested data was not found.", statusCode = "500")
 
         return entry_of_interest.to_json()
 
 @name_space.route('/get_entry/<string:date_1>_until_<string:date_2>')
 class GetRangeOfDates(Resource):
     
-    @app.doc(responses={ 200: 'OK', 500: 'Pelo menos uma das datas não foi encontrada no dataset' }, 
-			 params={ 'date_1': 'Especifica a primeira data desejada no formato dd/mm/yyyy',
-                      'date_2': 'Especifica a primeira data desejada no formato dd/mm/yyyy',})
+    @app.doc(responses={ 200: 'OK', 500: 'At least one of the dates was not found.' }, 
+			 params={ 'date_1': 'Specify the first date in the format dd/mm/yyyy',
+                      'date_2': 'Specify the first date in the format dd/mm/yyyy',})
     def get(self, date_1, date_2):
-        """ Retorna o update para uma data específica
+        """ Returns the updates for a specific range of dates
 
-            Deve ser feito no formato dia-mês-ano. Por exemplo /get_entry/01-04-2020_until_03_04_2020
+            Should be asked in the following format: dd-mm-yyyy. For example: /get_entry/01-04-2020_until_05-04-2020
+
+            Returns a dict with the following fomat: {index -> {column -> value}}
+
+            PT: Retorna o update para um intervalo de dados específico.
+
+            Deve ser feito no formato dd-mm-yyyy. Por exemplo /get_entry/01-04-2020_until_03_04_2020
 
             Retorna um dicionário em formato JSON do tipo: {index -> {column -> value}}
 
@@ -90,7 +110,7 @@ class GetRangeOfDates(Resource):
         entry_date_2 = df.loc[df.data == date_2]
 
         if (entry_date_1.shape[0] == 0) or (entry_date_2.shape[0] == 0):
-            return make_response('Pelo menos uma das datas não foi encontrada no dataset', 404)
+            return make_response('At least one of the dates was not found.', 404)
 
         entry_of_interest = df.iloc[entry_date_1.index[0]: entry_date_2.index[0], :]
 
@@ -101,7 +121,9 @@ class GetStatus(Resource):
     @app.doc(responses={ 200: 'Server is OK'})
 
     def get(self):
-        """ Retorna o Estado da API
+        """ Returns the state of the API
+
+            PT: Retorna o estado da API
         """
 
         return {
